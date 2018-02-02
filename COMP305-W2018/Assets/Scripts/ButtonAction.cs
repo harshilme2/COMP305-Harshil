24f74fd62365f98823cf5bf566c149530f1125b2
Harshil Mehta <harshilme2@gmail.com>
2018-02-02 14:20:25 -0500

    PracticeWeek2
    Week 2 Practice for Lab 2 and 3

---
+++ COMP305-W2018/Assets/Scripts/ButtonAction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAction : MonoBehaviour {
+
+    public GameObject CaptainFalcon;
+    public Sprite firstSprite;
+    public Sprite secondSprite;
+    
+    private SpriteRenderer sRend;
+    private Rigidbody2D rBody;
+	// Use this for initialization
+	void Start () {
+        //Debug.Log(somenumber);
+        rBody = CaptainFalcon.GetComponent<Rigidbody2D>();
+        sRend = CaptainFalcon.GetComponent<SpriteRenderer>();
+
+        rBody.velocity = new Vector2(2, 0);
+    }
+
+    // executes when mouse down event is fired on the attached object
+    void OnMouseDown()
+    {
+        rBody.velocity *= -1;
+        if(sRend.sprite == firstSprite)
+        {
+            sRend.sprite = secondSprite;
+        }
+        else
+        {
+            sRend.sprite = firstSprite;
+        }
+        
+    }
+
+}
