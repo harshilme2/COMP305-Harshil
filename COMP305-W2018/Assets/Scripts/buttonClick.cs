ff5efc09e979b6120f384bfe8c59888578a14b87
Harshil Mehta <harshilme2@gmail.com>
2018-02-04 19:44:38 -0500

    Lab 2
    Push

---
+++ COMP305-W2018/Assets/Scripts/buttonClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonClick : MonoBehaviour {
+
+    public GameObject cBoy;
+    //public Sprite redSprite;
+    //public Sprite blueSprite;
+    //public Sprite greenSprite;
+    public Sprite newSprite;
+    private SpriteRenderer sRend;
+    
+    // Use this for initialization
+    void Start () {
+       
+        sRend = cBoy.GetComponent<SpriteRenderer>();
+
+    }
+
+    void OnMouseDown()
+    {
+        sRend.sprite = newSprite;
+
+    }
+
+}
